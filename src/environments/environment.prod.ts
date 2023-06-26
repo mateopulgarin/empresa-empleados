22d2c506e16bf225378dcc7d73f38ec65d53229f
mateopulgarin <136388975+mateopulgarin@users.noreply.github.com>
2023-06-26 17:00:09 -0500

    new-empleados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCy07vquk9FKflAWGDYJXIM3Ua_rlk0-0U",
+    authDomain: "empleados-342ce.firebaseapp.com",
+    projectId: "empleados-342ce",
+    storageBucket: "empleados-342ce.appspot.com",
+    messagingSenderId: "944924156569",
+    appId: "1:944924156569:web:cdafcf31ba17cc91e58e08"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCy07vquk9FKflAWGDYJXIM3Ua_rlk0-0U",
+    authDomain: "empleados-342ce.firebaseapp.com",
+    projectId: "empleados-342ce",
+    storageBucket: "empleados-342ce.appspot.com",
+    messagingSenderId: "944924156569",
+    appId: "1:944924156569:web:cdafcf31ba17cc91e58e08"
+  }
 };
 
 /*
